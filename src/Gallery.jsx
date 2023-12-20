f9bdfd1fcf618920be08f8d3af95724a4d8261a1
Joshua Dodofoli <mac@Macs-MacBook-Air.local>
2023-12-20 00:31:23 +0000

    initial commit

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -16,13 +16,11 @@ const Gallery = () => {
 
       <div className="bg-[url('public/images/mobile/image-photography.jpg')] lg:h-auto h-[40rem] lg:bg-[url('public/images/desktop/image-photography.jpg')] lg:bg-cover lg:bg-center w-auto bg-cover bg-center">
         <div className="flex items-center justify-end mt-[25rem] flex-col px-[2rem] text-center">
-          <h3 className="font-[fraunces] text-[1.7rem] py-[1rem] text-Dark-desaturated-cyan">
-            Graphic Design
+          <h3 className="font-[fraunces] text-[1.7rem] py-[1rem] text-Dark-blue">
+            Photography
           </h3>
-          <p className="font-[barlow] text-Dark-desaturated-cyan font-[1.1rem] lg:mx-[9rem]">
-            Great design makes you memorable. We deliver artwork that
-            underscores your brand message and captures potential clients'
-            attention.
+          <p className="font-[barlow] text-Dark-blue font-[1.1rem] lg:mx-[9rem]">
+            Increase your credibility by getting the most stunning high-quality photos that improve your business image. 
           </p>
         </div>
       </div>
